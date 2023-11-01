0a0a8bec884437b33134706194da0dbdae65252f
CodingVirus <twominyong@gmail.com>
2023-11-01 21:04:31 +0900

    연습문제 커밋

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +39,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+                    //GreetingImage(message = stringResource(R.string.happy_birthday_text),
+                        //stringResource(R.string.signature_text))
+                    TaskCheck(stringResource(R.string.first_text),
+                        stringResource(R.string.second_text))
                 }
             }
         }
@@ -61,7 +68,7 @@ fun GreetingText(message: String, from : String ,modifier: Modifier = Modifier) 
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
@@ -86,10 +93,72 @@ fun GreetingImage(message : String, from : String, modifier: Modifier = Modifier
     }
 }
 
+@Composable
+fun JetpackTutorial(modifier: Modifier = Modifier) {
+    Column() {
+        Image(
+            painter = painterResource(R.drawable.bg_compose_background),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.title_text),
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.introduction_Text),
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = stringResource(R.string.main_text),
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun TaskCheck(
+    firstText : String,
+    secondText : String,
+    modifier: Modifier = Modifier
+) {
+    val image = painterResource(R.drawable.ic_task_completed)
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Image(painter = image, contentDescription = null)
+
+        Text(
+            text = firstText,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier
+                .padding(top = 8.dp, bottom = 8.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+
+        Text(
+            text = secondText,
+            fontSize = 16.sp,
+            modifier = modifier
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
 @Preview(showBackground =  false)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage("Happy Birthday Sam!", "From Emma")
+        //GreetingImage("Happy Birthday Sam!", "From Emma")
+        TaskCheck(stringResource(R.string.first_text),
+            stringResource(R.string.second_text))
     }
 }
